67820af2fd5e68c8d946a6815b4d2b4155d5db46
Theta_Mario <hadesh0815@hotmail.com>
2024-01-07 20:31:58 +0800

    updates

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-let elapsedSeconds = 0;
+let elapsedSeconds = localStorage.getItem("elapsedSeconds")
+  ? parseInt(localStorage.getItem("elapsedSeconds"))
+  : 0;
 
 function updateTimer() {
-  // 计算天、小时、分钟和秒
   const days = Math.floor(elapsedSeconds / (24 * 60 * 60));
   const hours = Math.floor((elapsedSeconds % (24 * 60 * 60)) / 3600);
   const minutes = Math.floor((elapsedSeconds % 3600) / 60);
   const seconds = elapsedSeconds % 60;
 
-  // 更新页面上的计时器
   document.getElementById(
     "timer"
   ).innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
-  // 增加逝去的秒数
   elapsedSeconds++;
+  localStorage.setItem("elapsedSeconds", elapsedSeconds);
 }
 
-// 初始调用一次，然后每秒更新一次
 updateTimer();
 setInterval(updateTimer, 1000);
 
