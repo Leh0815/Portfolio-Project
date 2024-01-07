0f3df62ff35bae24dfb1f0cdf3efae7215132d66
Theta_Mario <hadesh0815@hotmail.com>
2024-01-07 17:12:31 +0800

    time function updated

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const targetDate = new Date("2024-01-31T00:00:00");
 
 function updateElapsedTime() {
-  // 当前时间
   const currentDate = new Date();
 
-  // 计算时间差
   const timeDifference = targetDate - currentDate;
 
-  // 转换为天、小时和秒
   const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
     (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
@@ -15,13 +12,11 @@ function updateElapsedTime() {
   const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-  // 更新页面上的时间段落
   document.getElementById(
     "time"
   ).innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 }
 
-// 初始调用一次，然后每秒更新一次
 updateElapsedTime();
 setInterval(updateElapsedTime, 1000);
 
