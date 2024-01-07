3de62dc2e883cf723f92f07694b237738526799f
Theta_Mario <hadesh0815@hotmail.com>
2024-01-07 17:11:45 +0800

    time function updated

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-function updateTimer() {
-  const currentTime = new Date();
-  const hours = currentTime.getHours();
-  const minutes = currentTime.getMinutes();
-  const seconds = currentTime.getSeconds();
+const targetDate = new Date("2024-01-31T00:00:00");
 
-  const formattedTime = `${padZero(hours)}:${padZero(minutes)}:${padZero(
-    seconds
-  )}`;
-  document.getElementById("time-container").innerText = formattedTime;
+function updateElapsedTime() {
+  // 当前时间
+  const currentDate = new Date();
+
+  // 计算时间差
+  const timeDifference = targetDate - currentDate;
+
+  // 转换为天、小时和秒
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  // 更新页面上的时间段落
+  document.getElementById(
+    "time"
+  ).innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 }
 
-function padZero(value) {
-  return value < 10 ? `0${value}` : value;
-}
-
-setInterval(updateTimer, 1000);
-
-updateTimer();
+// 初始调用一次，然后每秒更新一次
+updateElapsedTime();
+setInterval(updateElapsedTime, 1000);
 
 function toggleImageAndParagraph(element) {
   var image = element.querySelector("img");
